163ab41041bfaa38cc6e3e1fb7a1f91b351ad594
hxni444 <107667722+hxni444@users.noreply.github.com>
2024-02-03 11:25:19 +0530

    com_1

---
+++ Nexu SMS/Entity/ContextClass.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Nexu_SMS.Repository;
+
+namespace Nexu_SMS.Entity
+{
+    public class ContextClass: DbContext
+    {
+        public IConfiguration configuration;
+        public ContextClass (IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public DbSet<AdmissionNo> admissionNos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("conString"));
+        }
+
+    }
+}
---
+++ Nexu SMS/Migrations/20240203054426_initialMigra.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Nexu_SMS.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialMigra : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AdmissionNoTable",
+                columns: table => new
+                {
+                    admissionNo = table.Column<string>(type: "varchar(30)", maxLength: 30, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AdmissionNoTable", x => x.admissionNo);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AdmissionNoTable");
+        }
+    }
+}
