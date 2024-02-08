bbddb38a34acd77d35e1a30e4576c3086288e967
hxni444 <107667722+hxni444@users.noreply.github.com>
2024-02-09 00:01:55 +0530

    update

---
+++ Login/LoginPage.jsx
@@ -0,0 +1,189 @@
+import React, { useRef, useState } from 'react';
+import axios from 'axios'
+import './Lstyle.css';
+import { Link, useNavigate } from 'react-router-dom';
+import { Alert } from 'react-bootstrap';
+
+const Login= () => {
+  
+  const userRef=useRef()
+    const passRef=useRef()
+    const navigate=useNavigate()
+    
+// function Log(){
+//     axios
+//     .get("http://localhost:5099/api/Users/GetAllUsers")
+//     .then((res)=>{
+
+//         let isValid=false
+//         for(let i=0;i<res.data.length;i++)
+//         {
+//             if(res.data[i].userName==userRef.current.value && res.data[i].password==passRef.current.value){
+//                 isValid=true
+//                 sessionStorage.setItem("uname",res.data[i].userName)
+//                 sessionStorage.setItem("uid",res.data[i].userId)
+//                 sessionStorage.setItem("role",res.data[i].role)
+//                 break;
+//             }
+//             else{
+//                 isValid=false
+//             }
+//         }
+//         if(isValid){
+//           let role=sessionStorage.getItem("role")
+//                 console.log("Success");
+//                 if(role=="student")
+//                 navigate("/studdashboard")
+//                 else if(role=="teacher")
+//                 navigate("/teacherdashboard")
+
+//         }
+//         else{
+//             console.log("Failed");
+//         }
+
+        
+// })
+// }
+
+
+const [user, setUser] = useState({ username: "", password: "" });
+const [err, setErr] = useState("");
+const Validate = (e) => {
+  // e.preventDefault();
+  axios
+    .post("http://localhost:5099/api/Users/Log_in", user)
+    .then((response) => {
+      console.log(response.data);
+      let validUser = response.data;
+      if (validUser != null) {
+        //set username in sessionstorage
+        sessionStorage.setItem("uid", validUser.userId);
+        sessionStorage.setItem("token", validUser.token);
+        if (validUser.role === "Admin") {
+          navigate("/admin-dashboard");
+        } else if (validUser.role === "Student") {
+          navigate("/studdashboard");
+        } else if (validUser.role === "Teacher") {
+          navigate("/teacherdashboard");
+        }
+        else{
+          setErr("invalid credentials")
+        }
+      } else {
+        
+      }
+    })
+    .catch((err) => {
+      console.log(err)
+      
+    });
+};
+
+
+  return (
+    
+    <div className='contact-page '> 
+        <div className='bg-dark text-light p-5 '>
+            <div className='container d-flex justify-content-center'>
+            <div className='containerlog'>
+                <h2 className='text-center '></h2>
+                
+     
+     <div className="cover">
+       <div className="front">
+         {/* <img src="../../utiles/images/frontImg.jpg" alt="Front Image" /> */}
+         <div className="text">
+           <span className="text-1">NEXUS SMS</span>
+          
+         </div>
+       </div>
+       <div className="back backImg">
+         {/* <img src="images/backImg.jpg" alt="Back Image" className="backImg" /> */}
+         <div className="text">
+           {/* <span className="text-1">Complete miles of journey <br /> with one step</span>
+           <span className="text-2">Let's get started</span> */}
+         </div>
+       </div>
+     </div>
+
+     
+     <div className="forms">
+       <div className="form-content">
+         <div >
+           <div className="title">Login</div>
+           {/*<form action="#">*/}
+             <div className="input-boxes">
+               <div className="input-box">
+                 <i className="fas fa-envelope"></i>
+                 <input type='text' placeholder="Enter your Username" value={user.username}
+                onChange={(e) =>
+                  setUser((prevstate) => ({
+                    ...prevstate,
+                    username: e.target.value,
+                  }))
+                } required />
+               </div>
+               <div className="input-box">
+                 <i className="fas fa-lock"></i>
+                 <input type="password" placeholder="Enter your password" value={user.password}
+                onChange={(e) =>
+                  setUser((prevstate) => ({
+                    ...prevstate,
+                    password: e.target.value,
+                  }))
+                } required />
+               </div>
+               <div className="text">
+                 <a href="#">Forgot password?</a>
+               </div>
+               <p className='text-danger text-center'>{err}</p>
+               <div className="button input-box">
+                 <input type="button" value="Submit" onClick={Validate} />
+                 {/* <button className='btn btn-primary' onClick={Validate}>Login</button> */}
+               </div>
+              
+               <Link to="/studregister">
+               <div className="text ">Don't have an account? <label htmlFor="flip">Sigup now</label></div>
+               </Link>
+             </div>
+           {/* </form> */}
+         </div>
+
+         {/* <div className={isLoginForm ? "signup-form" : "signup-form active-form"}>
+           <div className="title">Signup</div>
+           <form action="#">
+             <div className="input-boxes">
+               <div className="input-box">
+                 <i className="fas fa-user"></i>
+                 <input type="text" placeholder="Enter your name" required />
+               </div>
+               <div className="input-box">
+                 <i className="fas fa-envelope"></i>
+                 <input type="text" placeholder="Enter your email" required />
+               </div>
+               <div className="input-box">
+                 <i className="fas fa-lock"></i>
+                 <input type="password" placeholder="Enter your password" required />
+               </div>
+               <div className="button input-box">
+                 <input type="submit" value="Submit" />
+               </div>
+               <div className="text sign-up-text">Already have an account? <label htmlFor="flip">Login now</label></div>
+             </div>
+           </form>
+         </div> */}
+       </div>
+            </div>
+        </div>
+    
+    {/* <div className="container"> */}
+     
+      </div>
+    </div>
+    </div>
+   
+  );
+};
+
+export default Login;
---
+++ StudentDashboard/StudentDashboard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import StudentProfile from './StudentProfile'
+
+export default function StudentDashboard() {
+  return (
+    <div>
+      <StudentProfile/>
+      </div>
+  )
+}
